1098841479acd6847dbd0d7327179a877754f30b
Robert Masen <robert.masen@smartthings.com>
2022-11-01 14:35:25 -0500

    add find_ip function

--- raw-mdns/src/main.rs
+++ raw-mdns/src/main.rs
@@ -1,26 +1,30 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use net2::unix::UnixUdpBuilderExt;
+use nix::net::if_::InterfaceFlags;
 
 const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
 const MULTICAST_PORT: u16 = 5353;
 const INADDR_ANY: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
-const MY_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 71);
+const MY_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 24);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ip = find_ip();
+    println!("IP: {ip}");
     let udp_socket = net2::UdpBuilder::new_v4()
         .unwrap()
         .reuse_address(true)
         .unwrap()
         .reuse_port(true)
         .unwrap()
-        .bind((INADDR_ANY, MULTICAST_PORT))
+        .bind((ip, MULTICAST_PORT))
         .unwrap();
-
-    udp_socket.set_multicast_loop_v4(false).unwrap();
+        udp_socket.set_multicast_loop_v4(false).unwrap();
     udp_socket
-        .join_multicast_v4(&MULTICAST_ADDR, &MY_IP)
+        .join_multicast_v4(&MULTICAST_ADDR, &ip)
         .unwrap();
+    let b_ip = udp_socket.local_addr().unwrap();
+    println!("Bound IP: {b_ip}");
 
     let mut builder = dns_parser::Builder::new_query(0, false);
     builder.add_question(
@@ -50,4 +54,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     Ok(())
+}
+
+
+fn find_ip() -> Ipv4Addr {
+    
+    for interface in nix::ifaddrs::getifaddrs().unwrap() {
+        // println!("{interface:#?}");
+        if interface.flags.contains(InterfaceFlags::IFF_RUNNING | InterfaceFlags::IFF_BROADCAST | InterfaceFlags::IFF_MULTICAST) {
+            if let Some(addr) = interface.address {
+                if let Some(v4) = addr.as_sockaddr_in() {
+                    return v4.ip().into()
+                }
+            }
+        }
+    }
+    panic!()
 }
